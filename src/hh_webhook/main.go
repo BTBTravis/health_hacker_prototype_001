458fc1beae51595137da7fd8c339975e120d1b59
Alexander Kalach <alexanderkalach@gmail.com>
2019-09-21 14:33:23 +0200

    update hhwebhook

--- src/hh_webhook/main.go
+++ src/hh_webhook/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"log"
 )
+
+type UrlVerification struct{
+	Token string	`json:"token"`
+	Challenge string `json:"challenge"`
+	Type string `json:"type"`
+}
 
 func Handler(context context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Printf("1. Processing Lambda request %s\n", request.RequestContext.RequestID)
@@ -16,10 +23,16 @@ func Handler(context context.Context, request events.APIGatewayProxyRequest) (ev
 		return events.APIGatewayProxyResponse{}, errors.New("Error: no query was provided in the HTTP body")
 	}
 
+	var v UrlVerification
+	if err := json.Unmarshal([]byte(request.Body), &v); err != nil {
+		log.Print("Error: Could not decode body!", err)
+	}
+
 	log.Printf("Request: ", request.Body)
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
+		Body: v.Challenge,
 	}, nil
 }
 
