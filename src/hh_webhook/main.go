6360b9fb5f4a063176069d2a64f96426a45d5b66
Travis Shears <btbtravis@gmail.com>
2019-09-21 19:09:00 +0200

    Init relay system

--- src/hh_webhook/main.go
+++ src/hh_webhook/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+        "regexp"
         "main/sender"
 	"encoding/json"
 	"errors"
@@ -11,9 +12,37 @@ import (
 )
 
 type UrlVerification struct{
-	Token string	`json:"token"`
-	Challenge string `json:"challenge"`
-	Type string `json:"type"`
+    Token string    `json:"token"`
+    Challenge string `json:"challenge"`
+    Type string `json:"type"`
+    Event SlackEvent `json:"event"`
+}
+
+type SlackEvent struct {
+    ClientMsgId string `json:"client_msg_id"`
+    Type string `json:"type"`
+    Text string `json:"text"`
+    User string `json:"user"`
+    Ts string `json:"ts"`
+    Team string `json:"team"`
+    Channel string `json:"channel"`
+    EventTs string `json:"event_ts"`
+    ChannelType string `json:"channel_type"`
+}
+
+type SlackUser struct{
+    ChannelId string
+    UserId string
+}
+
+var Travis = SlackUser{
+    ChannelId: "DN7L5L8AW",
+    UserId: "UMYDK4Q66",
+}
+
+var Alex = SlackUser{
+    ChannelId: "DNL27EDE0",
+    UserId: "UND2UQZB9",
 }
 
 func Handler(context context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -31,7 +60,19 @@ func Handler(context context.Context, request events.APIGatewayProxyRequest) (ev
 
 	log.Printf("Request: ", request.Body)
 
-        sender.SendMessage("Hello there cowboy")
+	if v.Event.User == Travis.UserId {
+	    sender.SendMessage(Alex.ChannelId, v.Event.Text)
+	} else if v.Event.User == Alex.UserId {
+            ogMsg := v.Event.Text
+            jumper, _ := regexp.MatchString("jump", ogMsg)
+
+            if jumper {
+                sender.SendMessage(Alex.ChannelId, "don't do it call: XXX-XXXX-XXXX")
+                sender.SendMessage(Travis.ChannelId, "WARNING: alex is on the edge")
+            } else {
+                sender.SendMessage(Travis.ChannelId, ogMsg)
+            }
+	}
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
--- src/hh_webhook/sender/sender.go
+++ src/hh_webhook/sender/sender.go
@@ -1,60 +1,23 @@
 package sender
 
 import (
-    "bytes"
-    "net/http"
     "log"
-    "encoding/json"
+    "net/http"
+    "net/url"
 )
 
+func SendMessage(channelId string, message string) {
+    params := url.Values{}
+    params.Add("text", message)
+    params.Add("channel", channelId)
+    params.Add("token", "xoxb-752798852689-756983711187-QPM8GGpBrrUV14NzWEIzR7om")
+    slackURL := "https://slack.com/api/chat.postMessage?" + params.Encode()
 
+    log.Println(slackURL)
+    resp, err := http.Get(slackURL)
 
-type SlackUser struct{
-    ChannelId string
-    UserId string
-}
-
-var Travis = SlackUser{
-    ChannelId: "DN7L5L8AW",
-    UserId: "UMYDK4Q66",
-}
-
-var Alex = SlackUser{
-    ChannelId: "DNLGGKBGD",
-    UserId: "UND2UQZB9",
-}
-
-
-func SendMessage(message string) {
-
-    type Msg struct{
-	Text string `json:"text"`
-	Channel string `json:"channel"`
-    }
-
-    var msg = Msg{
-        Text: message,
-        Channel: Travis.ChannelId,
-    }
-
-    jsonValue, _ := json.Marshal(msg)
-    buf := new(bytes.Buffer)
-    json.NewEncoder(buf).Encode(jsonValue)
-
-
-    // TODO move secret to env var
-    var bearer = "Bearer " + "xoxb-752798852689-756983711187-MfD6lVvnEY5hVaBRrpjrrWSs"
-    client := &http.Client{}
-    req, err := http.NewRequest("POST", "https://slack.com/api/chat.postMessage", buf)
-    req.Header.Set("Authorization", bearer)
-    req.Header.Set("User-Agent", "application/json")
-    log.Println(req)
-    resp, err := client.Do(req)
     if err != nil {
         log.Println(err)
     }
     log.Println(resp)
-
-    //res, err := http.Post("https://slack.com/api/chat.postMessage", "application/json", bytes.NewBuffer(bytesRepresentation))
-    //res, err := http.Post("https://hooks.slack.com/services/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", "application/json", bytes.NewBuffer(bytesRepresentation))
 }
